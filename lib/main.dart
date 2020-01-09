dab097c43964125ea250d4209006e7e59028eae4
carolina <carolina.louzada@dcomp.ufs.br>
2020-01-09 20:37:27 -0300

    adicao dos controladores e logica do imc

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,34 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  // controladores para captar dados do formulario
+  TextEditingController weightController = TextEditingController();
+  TextEditingController heightController = TextEditingController();
+  String _info = " Informe seus dados!";
+
+  void _resetFields(){
+    setState(() {
+      weightController.text = "";
+      heightController.text = "";
+      _info = "Informe seus dados!";
+    });
+    
+  }
+  void _calculate(){
+    setState(() {
+    double weight = double.parse(weightController.text);
+    double height = double.parse(heightController.text)/100;
+    double imc = weight / (height * height);
+    if(imc < 18.6) _info = "IMC = (${imc.toStringAsPrecision(3)} Abaixo do peso!";
+    else if(imc>= 18.6 && imc < 24.9) _info = "IMC = ${imc.toStringAsPrecision(3)} Peso Ideal!";
+    else if(imc >= 24.9 && imc < 29.9) _info = "IMC = ${imc.toStringAsPrecision(3)} Levemente Acima do peso!";
+    else if(imc >= 29.9 && imc < 34.9) _info = " IMC = ${imc.toStringAsPrecision(3)} Obesidade Grau I";
+    else if(imc >= 34.9 && imc < 39.9) _info = "IMC = ${imc.toStringAsPrecision(3)} Obesidade Grau II";
+    else if(imc >= 39.9) _info = "IMC = ${imc.toStringAsPrecision(3)} Obesidade Grau III";
+
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,7 +50,9 @@ class _HomeState extends State<Home> {
         actions: <Widget>[ // adiciona botoes e acoes na appbarra
           IconButton(
             icon: Icon(Icons.refresh),
-            onPressed: (){}
+            onPressed: (){
+              _resetFields();
+            }
             ) 
         ],
       ),
@@ -41,6 +71,7 @@ class _HomeState extends State<Home> {
               ),
               textAlign: TextAlign.center,
               style: TextStyle(color: Colors.deepPurple, fontSize: 25.0),
+              controller: weightController,
             ),
             TextField(
               keyboardType: TextInputType.number,
@@ -50,13 +81,14 @@ class _HomeState extends State<Home> {
                 ),
               textAlign: TextAlign.center,
               style: TextStyle(color: Colors.deepPurple, fontSize: 25.0),
+              controller: heightController,
             ),
             Padding(
               padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
               child: Container(
                 height: 50.0,
                 child: RaisedButton(
-                  onPressed: (){},
+                  onPressed: _calculate(),
                   child: Text(
                     "Calcular IMC",
                     style: TextStyle(color: Colors.white, fontSize: 25.0),
@@ -66,7 +98,7 @@ class _HomeState extends State<Home> {
               ),
             ),
             Text(
-              "Info",
+              _info,
               textAlign: TextAlign.center,
               style: TextStyle(
                 color: Colors.deepPurple, fontSize: 25.0,
